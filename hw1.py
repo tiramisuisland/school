fb16f27b8ff1028704ab544dc5456dfd0ab85226
tiramisuisland <tiramisuisland@gmail.com>
2024-09-30 11:25:07 +0800

    hw1

---
+++ hw1.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x1 = np.linspace(0.0,2*np.pi)
+y1 = np.sin(x1)
+
+x2 = np.linspace(0.0,2*np.pi)
+y2 = np.cos(y1)
+
+plt.plot(x1, y1, 'r', label='sin')
+
+plt.plot(x2, y2, 'b-o', label='cos')
+
+plt.legend()
+plt.show()
